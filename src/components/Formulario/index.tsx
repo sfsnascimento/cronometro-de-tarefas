c8f6f641f21851c1b70044d9e976a10869b4a865
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-13 22:38:34 -0300

    estilizando componente Formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Botao from '../Botao';
+import './style.scss';
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form>
-        <div>
+      <form className="novaTarefa">
+        <div className="inputContainer">
           <label htmlFor="tarefa">
             Adicione um novo estudo
           </label>
@@ -16,7 +17,7 @@ class Formulario extends React.Component {
             required
           />
         </div>
-        <div>
+        <div className="inputContainer">
           <label htmlFor="tempo">
             Tempo
           </label>
