ea7e0787b30373a389e44d7cb8b46b79d42abe9b
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-15 15:55:57 -0300

    adicionando useEffect no cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../interfaces/ITarefa";
 
-export default function Cronometro() {
+interface IProps {
+  selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: IProps) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado])
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
