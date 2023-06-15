4b2dbfe8269f25f9ddcaf757421bdeba6e348ed1
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-15 12:12:27 -0300

    modificando componente Item para receber prop selecionaTarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,8 +1,22 @@
+import { ITarefa } from '../../../interfaces/ITarefa';
 import style from '../Lista.module.scss';
 
-export default function item({ tarefa, tempo }: { tarefa: string, tempo: string}) {
+interface IProps extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: IProps) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      onClick={() => selecionaTarefa({
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id
+      })}
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
