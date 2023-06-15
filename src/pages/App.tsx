8a7495da59ac9852101f2f2d11850ce5b8f39375
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-15 12:11:23 -0300

    criando função selecionaTarefa e passando para o componente Lista

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,11 +7,20 @@ import { ITarefa } from '../interfaces/ITarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
 
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas}/>
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
       <Cronometro />
     </div>
   );
